52640c951e1d966cf115c9934c45dfbd471a696c
TSanni <tomas.sanni@gmail.com>
2021-11-02 20:09:57 -0500

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Tomas Sanni on 11/2/21.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
