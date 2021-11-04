8b98e8a91320f118b2f1dcdc4eea0439329bfb03
TSanni <tomas.sanni@gmail.com>
2021-11-03 21:46:45 -0500

    Add WebKit and NavigationLink to display information when user clicks on post

--- HackerNews/Views/ContentView.swift
+++ HackerNews/Views/ContentView.swift
@@ -15,11 +15,15 @@ struct ContentView: View {
         NavigationView {
             
             List(networkManager.posts) { post in
-                HStack {
-                    Text(String(post.points))
-                    Text(post.title)
+                NavigationLink(destination: DetailView(url: post.url)) {
                     
+                    HStack {
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
                 }
+                
+                
             }
             .navigationBarTitle("Hacker News")
         }
